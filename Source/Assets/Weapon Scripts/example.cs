97e8faca2f5ff92326ea8a963293e022c4d32136
Chris Chin <religionman0@gmail.com>
2012-11-01 22:01:58 -0400

    Projectile
    Continued projectile work

--- Source/Assets/Weapon Scripts/example.cs
+++ Source/Assets/Weapon Scripts/example.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class example : MonoBehaviour {
-    public GameObject projectile;
+    public GameObject shot;
+	public GameObject suction;
     public float fireRate = 0.5F;
 	public float speed = 20F;
     private float nextFire = 0.0F;
     void Update() {
-        if (Input.GetButton("Fire2") && Time.time > nextFire) {
+        if (Input.GetButton("Fire1") && Time.time > nextFire) {
             nextFire = Time.time + fireRate;
-            GameObject clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            GameObject clone = Instantiate(shot, transform.position, transform.rotation) as GameObject;
+			clone.rigidbody.velocity = transform.TransformDirection(Vector3.forward * speed);
+        }
+		if (Input.GetButton("Fire2") && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            GameObject clone = Instantiate(suction, transform.position, transform.rotation) as GameObject;
 			clone.rigidbody.velocity = transform.TransformDirection(Vector3.forward * speed);
         }
     }
